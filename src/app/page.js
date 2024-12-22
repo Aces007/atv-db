6c3ecf4507efb6e614f019cd5197c15ddc65b162
Ace Lawrence Zuñiga Clavano <92563567+Aces007@users.noreply.github.com>
2024-12-23 01:40:20 +0800

    added new page directories and modifications

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Home from "./components/Home";
+import Login from "./components/UserManagement/Login";
 
 export default function HomePage() {
   return (
     <div>
       <Home />
+      <Login />
     </div>
   );
 }
