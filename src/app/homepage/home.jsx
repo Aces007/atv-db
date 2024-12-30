224067aac9cb4d733c3e4b162b241ab5ee79f7f7
Ace Lawrence Zuñiga Clavano <92563567+Aces007@users.noreply.github.com>
2024-12-31 01:11:41 +0800

    creating design systems

--- src/app/homepage/home.jsx
+++ src/app/homepage/home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     return( 
         <div className="home_cont min-h-screen w-full bg-white ">
             <div className="home_content flex flex-col h-full">
-                <div className="header px-12 py-6 flex flex-row items-center justify-between">
+                <div className="header px-max py-lg flex flex-row items-center justify-between">
                     <div className="header_left">
                         <Link href={'./homepage/'}><img src="./images/logo.png" alt="ATV Logo" className="logo_img"/></Link>
                         <div className="logo_txt flex flex-col relative top-2">
@@ -24,10 +24,10 @@ const Home = () => {
                         </div>
                     </div>
 
-                    <div className="header_right flex flex-row items-center gap-10">
+                    <div className="header_right flex flex-row items-center gap-max">
                         <a href="#about" className="about_btns font-Montserrat font-bold uppercase">About</a>
                         <a href="https://www.pup.edu.ph/ous/" target="_blank" className="about_btns font-Montserrat font-bold uppercase">PUP OUS</a>
-                        <Link href={'./loginpage/'}><button className="login_btn px-5 py-2 font-Montserrat font-bold">LOGIN</button></Link>
+                        <Link href={'./loginpage/'}><button className="login_btn px-lg py-sm font-Montserrat font-bold">LOGIN</button></Link>
                     </div>
                 </div>
 
@@ -37,14 +37,14 @@ const Home = () => {
                     </div>
 
                     <div className="body_right p-8 flex flex-col align-left justify-around gap-28 w-min">
-                        <h1 className="right_txt font-Cinzel font-black uppercase text-4xl text-white opacity-80">Open Minds with Open Access</h1>
+                        <h1 className="right_txt font-Cinzel font-black uppercase text-med text-white opacity-80">Open Minds with Open Access</h1>
 
                         <div className="search_bar_cont">
                             <div className="search_bar_content flex flex-row">
                                 <div className="dropdown">
                                     <Dropdown>
                                         <DropdownTrigger>
-                                            <div className="drop_btn flex flex-row items-center gap-5 py-3 px-5 border cursor-pointer">
+                                            <div className="drop_btn flex flex-row items-center gap-min py-med px-lg border cursor-pointer">
                                                 <Image src="/images/materials/SVGs/down.svg" alt="dropdownBtn" width={30} height={30} />
                                                 <p className="drop_btnTxt font-Montserrat text-black">All Fields</p>
                                             </div>
@@ -60,7 +60,7 @@ const Home = () => {
                                     </Dropdown>
                                 </div>
                                 <div className="search_field">
-                                    <input type="text" placeholder="What are you looking for?..." className="search_field_bar h-14 p-5"/>
+                                    <input type="text" placeholder="What are you looking for?..." className="search_field_bar h-med p-lg"/>
                                 </div>
                                 <div className="search_btn">
                                     <button className="searchBtn">
