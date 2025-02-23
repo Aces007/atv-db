a94cb9787a250e80db811bf3b8f4b0066f722c58
Ace Lawrence Zuñiga Clavano <92563567+Aces007@users.noreply.github.com>
2025-02-23 19:07:58 +0800

    referenced profile page (unfinished)

---
+++ src/app/profilepage/page.jsx
@@ -0,0 +1,56 @@
+"use client";
+
+import {  Dropdown,  DropdownTrigger,  DropdownMenu,  DropdownSection,  DropdownItem} from "@nextui-org/dropdown";
+import Image from "next/image";
+import Link from "next/link";
+import Header from "../header/page";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock, faGlobe } from "@fortawesome/free-solid-svg-icons";
+
+
+const Profile = () => {
+    return (
+        <div className="profile_cont">
+            <div className="profile_content">
+                <Header />
+
+                <div className="profile_info flex flex-row justify-between mx-16 my-4 tracking-min">
+                    <div className="left_panel flex flex-col gap-6">
+                        <h1 className="font-Red_Hat_Display uppercase font-extrabold text-sm">Your Account</h1>
+
+                        <div className="user_basic flex flex-row gap-4">
+                            <Image src="/images/materials/placeholderImg.png" alt="Placeholder Image" width={50} height={50} className="placeHolderImg" />
+
+                            <div className="user_basic_info">
+                                <h1 className="font-Red_Hat_Display font-bold text-min">Ace Lawrence Z. Clavano</h1>
+                                <h1 className="font-Red_Hat_Display text-min">2021-05773-MN-0</h1>
+                            </div>
+                        </div>
+
+                        <div className="profile_btns flex flex-col items-start justify-around gap-5">
+                            <button className="profile_menu font-Montserrat text-sm flex flex-row items-center gap-4"><FontAwesomeIcon icon={faUser} size="1x" className="profileSVGs" />Personal Info</button>
+                            <button className="profile_menu font-Montserrat text-sm flex flex-row items-center gap-4"><FontAwesomeIcon icon={faLock} size="1x" className="profileSVGs" />Security</button>
+                            <Link href={"https://sis8.pup.edu.ph/"} target="_blank"><button className="profile_menu font-Montserrat text-sm flex flex-row items-center gap-4"><FontAwesomeIcon icon={faGlobe} size="1x" className="profileSVGs" />PUP SIS</button></Link>
+                        </div>
+                    </div>
+                    
+                    <div className="right_panel">
+                        <img src="/images/materials/streetart.jpg" alt="PUP Street Art" className="street_art" />
+
+                        <div className="info_content">
+                            <div className="info_preview">
+
+                            </div>
+
+                            <div className="edit_profile">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
