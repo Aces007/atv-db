13d52cd63f335127dea69e922f4636b7910e1880
Ace Lawrence Zuñiga Clavano <92563567+Aces007@users.noreply.github.com>
2025-03-04 16:43:04 +0800

    information from database reflecting on interface

--- src/app/profilepage/page.jsx
+++ src/app/profilepage/page.jsx
@@ -10,11 +10,43 @@ import { faUser, faLock, faGlobe, faBook } from "@fortawesome/free-solid-svg-ico
 import PersonalInfo from "../personalinfopanel/page";
 import Security from "../securitypanel/page";
 import MyLibrary from "../mylibrarypanel/page";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useWebsiteContext } from "../WebsiteContext";
 
 
 const Profile = () => {
     const [selectedSection, setSelectedSection] = useState('PersonalInfo');
+    const { fetchUserInfo, user } = useWebsiteContext();
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [birthdate, setBirthdate] = useState(null);
+    const [age, setAge] = useState(null);
+    const [studentNumber, setStudentNumber] = useState("");
+
+    useEffect(() => {
+        if (!user || !user.id) {
+            console.warn("User is not available yet.");
+            return;
+        }
+
+        console.log('userId in UserProfile:', user.id); 
+
+        const getUserData = async () => {
+            const userData = await fetchUserInfo(user.id);
+            if (!userData) {
+                console.error("User data not found.");
+                return;
+            }
+
+            setName(userData.full_name);
+            setEmail(userData.userEmail);
+            setBirthdate(userData.userBirthdate);
+            setAge(userData.userAge);
+            setStudentNumber(userData.userStudentNumber)
+        };
+
+        getUserData();
+    }, [user]);
 
     return (
         <div className="profile_cont">
@@ -29,8 +61,8 @@ const Profile = () => {
                             <Image src="/images/materials/placeholderImg.png" alt="Placeholder Image" width={50} height={50} className="placeHolderImg" />
 
                             <div className="user_basic_info">
-                                <h1 className="font-Red_Hat_Display font-bold text-min">Ace Lawrence Z. Clavano</h1>
-                                <h1 className="font-Red_Hat_Display text-min">2021-05773-MN-0</h1>
+                                <h1 className="font-Red_Hat_Display font-bold text-min">{name}</h1>
+                                <h1 className="font-Red_Hat_Display text-min">{studentNumber}</h1>
                             </div>
                         </div>
 
