7204505f7bb57777f052375739023e76090be54a
Ace Lawrence Zuñiga Clavano <92563567+Aces007@users.noreply.github.com>
2025-03-08 19:06:54 +0800

    fixing issues

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://ehwzmnpmtukqybibgwuk.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVod3ptbnBtdHVrcXliaWJnd3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0MzYxMjksImV4cCI6MjA1MDAxMjEyOX0.6zq77zrf8aa2AYR-VTs8fgSALVTEIbOjM4pcNXQR9Xw"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
