c582db6dcbd5c1515c9b6d02cc563ecf351209d9
Ace Lawrence Zuñiga Clavano <92563567+Aces007@users.noreply.github.com>
2025-03-10 19:33:22 +0800

    upload page done

--- src/app/footer/page.jsx
+++ src/app/footer/page.jsx
@@ -17,11 +17,11 @@ const Footer = () => {
                         <h1 className="font-Red_Hat_Display font-bold text-white">QUICK LINKS</h1>
 
                         <div className="link_list flex flex-col justify-evenly gap-3">
-                            <a href={'./aboutpage/'} className="font-Red_Hat_Display font-medium text-white text-opacity-80">About ATV</a>
-                            <a href={'https://www.pup.edu.ph/about/'} className="font-Red_Hat_Display font-medium text-white text-opacity-80">About PUP</a>
-                            <a href={'https://www.pup.edu.ph/ous/'} className="font-Red_Hat_Display font-medium text-white text-opacity-80">PUP OUS</a>
-                            <a href={'https://sis8.pup.edu.ph/'} className="font-Red_Hat_Display font-medium text-white text-opacity-80">PUP SIS</a>
-                            <a href={'https://emabini.pup.edu.ph/'} className="font-Red_Hat_Display font-medium text-white text-opacity-80">PUP eMabini</a>
+                            <a href={'./aboutpage/'} className="footer_links font-Red_Hat_Display font-medium text-white text-opacity-80">About ATV</a>
+                            <a href={'https://www.pup.edu.ph/about/'} className="footer_links font-Red_Hat_Display font-medium text-white text-opacity-80">About PUP</a>
+                            <a href={'https://www.pup.edu.ph/ous/'} className="footer_links font-Red_Hat_Display font-medium text-white text-opacity-80">PUP OUS</a>
+                            <a href={'https://sis8.pup.edu.ph/'} className="footer_links font-Red_Hat_Display font-medium text-white text-opacity-80">PUP SIS</a>
+                            <a href={'https://emabini.pup.edu.ph/'} className="footer_links font-Red_Hat_Display font-medium text-white text-opacity-80">PUP eMabini</a>
                         </div>
                     </div>
 
@@ -29,10 +29,10 @@ const Footer = () => {
                         <h1 className="font-Red_Hat_Display font-bold text-white">ONLINE SERVICES</h1>
 
                         <div className="link_list flex flex-col justify-evenly gap-3">
-                            <a href={'https://pupsinta.freshservice.com/support/home'} className="font-Red_Hat_Display font-medium text-white text-opacity-80">PUP SINTA</a>
-                            <a href={'https://outlook.office.com/'} className="font-Red_Hat_Display font-medium text-white text-opacity-80">PUP WebMail</a>
-                            <a href={'http://www.pup.edu.ph/iapply/'} className="font-Red_Hat_Display font-medium text-white text-opacity-80">PUP iApply</a>
-                            <a href={'https://www.pup.edu.ph/jobpost/'} className="font-Red_Hat_Display font-medium text-white text-opacity-80">Jobs for PUPians</a>
+                            <a href={'https://pupsinta.freshservice.com/support/home'} className="footer_links font-Red_Hat_Display font-medium text-white text-opacity-80">PUP SINTA</a>
+                            <a href={'https://outlook.office.com/'} className="footer_links font-Red_Hat_Display font-medium text-white text-opacity-80">PUP WebMail</a>
+                            <a href={'http://www.pup.edu.ph/iapply/'} className="footer_links font-Red_Hat_Display font-medium text-white text-opacity-80">PUP iApply</a>
+                            <a href={'https://www.pup.edu.ph/jobpost/'} className="footer_links font-Red_Hat_Display font-medium text-white text-opacity-80">Jobs for PUPians</a>
                         </div>
                     </div>
                 </div>
