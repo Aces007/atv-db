2b4c4e3ea2588d4059da2915c79c612207a9f4fd
Ace Lawrence Zuñiga Clavano <92563567+Aces007@users.noreply.github.com>
2025-06-25 12:47:16 +0800

    debugging errors

--- src/app/article_journalpage/page.jsx
+++ src/app/article_journalpage/page.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
@@ -5,7 +7,7 @@ const ArticleJournalPageClient = dynamic(() => import("./client"), {
   ssr: false, // Important: disables server-side rendering so useSearchParams can be used safely
 });
 
-export default function ArticleJournalPageWrapper() {
+export default function ArticleJournalPage() {
   return (
     <Suspense fallback={<div>Loading article...</div>}>
       <ArticleJournalPageClient />
